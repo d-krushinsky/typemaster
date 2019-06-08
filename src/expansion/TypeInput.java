8800d733c042da813cbc86a0ad51831c44ce3ed4
dyuzz16 <ciclopej@gmail.com>
2019-06-08 18:54:53 +0300

    Typing

--- src/expansion/TypeInput.java
+++ src/expansion/TypeInput.java
@@ -1,7 +1,41 @@
 package expansion;
 
+import java.awt.event.KeyEvent;
+
 import nightingale.input.NInput;
 
 public abstract class TypeInput extends NInput{
+	
+	protected String typedString = "";
+	protected String currentString = "";
+	protected boolean canTyping = false;
+	
+	public String getTypedString() {
+		return typedString;
+	}
+	
+	public String getCurrentString() {
+		return currentString;
+	}
+	
+	public void typingOff() { canTyping = false; }
+	public void typingOn()  { canTyping = true;  }
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		super.keyPressed(e);
+		if(canTyping) {
+			if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+				typedString = currentString;
+				currentString = "";
+			}else if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+				if(currentString.length() > 0) {
+					currentString = currentString.substring(0, currentString.length()-1);
+				}
+			}else {
+				currentString += e.getKeyChar();
+			}
+		}
+	}
 	
 }
