352453956f3524de78265a25c913f3e9a31787b3
dyuzz16 <ciclopej@gmail.com>
2019-06-10 19:05:52 +0300

    Add survive pre settings UI

--- src/game/state/ModeSelection.java
+++ src/game/state/ModeSelection.java
@@ -1,14 +1,11 @@
 package game.state;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 
-import game.Input;
 import game.TypeMaster;
 import game.resources.Fonts;
-import game.resources.Sprites;
 import game.state.listener.ModeSelectionListener;
 import nightingale.state.NState;
 import nightingale.ui.NActionListener;
@@ -19,20 +16,40 @@ import nightingale.ui.NUIGroup;
 
 public class ModeSelection implements NState{
 
-	protected NActionListener listener = new ModeSelectionListener();
+	protected NActionListener listener = new ModeSelectionListener(this);
 	
 	public NUIGroup ui = new NUIGroup();
+	public NUIGroup ssUI = new NUIGroup(); //Survive settings user interface
+	
+	public boolean survive = false;
+	
+	public int speed = 1;
 	
 	public ModeSelection() {
 		ui.addElement("TITLE", new NLabel("PLAY", 0, 40, 160, 40));
 		ui.addElement("SURVIVE", new NButton("Survive", 0, 140, 90, 50));
 		ui.addElement("TRAINING", new NButton("Training", 0, 200, 105, 50));
 		ui.addElement("BACK", new NButton("Back", 0, 300, 70, 40));
+		ssUI.addElement("Back_to_selection", new NButton("Back", 600, 340, 70, 40));
+		ssUI.addElement("play", new NButton("Play", 450, 240, 80, 50));
+		ssUI.addElement("speed", new NLabel("Speed: "+speed, 95, 100, 190, 25));
+		ssUI.addElement("down_speed", new NButton("Easier", 95, 130, 90, 40));
+		ssUI.addElement("up_speed", new NButton("Harder", 195, 130, 90, 40));
 		
 		ui.setActionListener(listener);
 		ui.setCamera(TypeMaster.uiCamera);
+		ssUI.setActionListener(listener);
+		ssUI.setCamera(TypeMaster.uiCamera);
 		
 		ui.getElements().forEach( (element) -> { 
+			if(element instanceof NButton) { 
+				((NButton)element).setFont(Fonts.uiFont);
+			}else if(element instanceof NLabel) {
+				((NLabel)element).setFont(Fonts.uiFont);
+			}
+		} );
+		
+		ssUI.getElements().forEach( (element) -> { 
 			if(element instanceof NButton) { 
 				((NButton)element).setFont(Fonts.uiFont);
 			}else if(element instanceof NLabel) {
@@ -47,17 +64,29 @@ public class ModeSelection implements NState{
 		for(NUIElement element : ui.getElements()) {
 			element.setX(TypeMaster.canvas.getWidth()/2-element.getWidth()/2);
 		}
-		Sprites.testAnimator.start();
+		for(NUIElement element : ssUI.getElements()) {
+			element.setX(TypeMaster.canvas.getWidth()/2-element.getWidth()/2);
+		}
+		survive = false;
+		ssUI.getElement("down_speed").setX(ssUI.getElement("speed").getX());
+		ssUI.getElement("up_speed").setX(
+			ssUI.getElement("speed").getX()+ssUI.getElement("speed").getWidth() - ssUI.getElement("up_speed").getWidth());
 	}
 
 	@Override
 	public void update() {
-		ui.perform(TypeMaster.in);
+		if(!survive)
+			ui.perform(TypeMaster.in);
+		else
+			ssUI.perform(TypeMaster.in);
 	}
 
 	@Override
 	public void draw(Graphics g, Graphics2D g2d, AffineTransform at) {
-		ui.draw(g);
+		if(!survive)
+			ui.draw(g);
+		else
+			ssUI.draw(g);
 	}
 	
 }
--- src/game/state/listener/ModeSelectionListener.java
+++ src/game/state/listener/ModeSelectionListener.java
@@ -1,10 +1,18 @@
 package game.state.listener;
 
 import game.TypeMaster;
+import game.state.ModeSelection;
 import nightingale.ui.NActionListener;
+import nightingale.ui.NLabel;
 import nightingale.ui.NUIElement;
 
 public class ModeSelectionListener implements NActionListener {
+	
+	private ModeSelection ms;
+	public ModeSelectionListener(ModeSelection ms) {
+		this.ms = ms;
+	}
+	
 	@Override
 	public void actionPerform(NUIElement element) {
 		if(element.getName() == "BACK") {
@@ -12,7 +20,15 @@ public class ModeSelectionListener implements NActionListener {
 		}else if(element.getName() == "TRAINING") {
 			TypeMaster.stateHandler.setState("TRAINING_STATE");
 		}else if(element.getName() == "SURVIVE") {
-			
+			ms.survive = true;
+		}else if(element.getName() == "Back_to_selection") {
+			ms.survive = false;
+		}else if(element.getName() == "down_speed") {
+			if(ms.speed > 1) ms.speed--;
+			((NLabel)ms.ssUI.getElement("speed")).setText("Speed: " + ms.speed);
+		}else if(element.getName() == "up_speed") {
+			if(ms.speed < 10) ms.speed++;
+			((NLabel)ms.ssUI.getElement("speed")).setText("Speed: " + ms.speed);
 		}
 	}
 }
