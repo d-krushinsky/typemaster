7e9e554e87a8a49659caa39d95f981dd07beae9a
dyuzz16 <ciclopej@gmail.com>
2019-06-29 21:37:55 +0300

    New Font

--- src/game/resources/Fonts.java
+++ src/game/resources/Fonts.java
@@ -6,17 +6,14 @@ public abstract class Fonts {
 
 	private static String symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz 1234567890!?()<>[]{}$+-*/\\=&#@:;.,|\"'^";
 	
-	public static NText uiFont = new NText(symbols, 12, 16, "/res/fonts/ui_font.png");
-	public static NText extraFont = new NText(symbols, 24, 32, "/res/fonts/extra_ui_font.png");
-	public static NText gameFont = new NText(symbols, 10, 14, "/res/fonts/game_font.png");
-	public static NText inputFont = new NText(symbols, 24, 32, "/res/fonts/extra_ui_font.png");
+	public static NText uiFont = new NText(symbols, 12, 12, "/res/fonts/font_new.png");
+	public static NText extraFont = new NText(symbols, 12, 12, "/res/fonts/font_new.png");
+	public static NText gameFont = new NText(symbols, 12, 12, "/res/fonts/font_new.png");
+	public static NText inputFont = new NText(symbols, 12, 12, "/res/fonts/font_new.png");
 	
 	static {
-		gameFont.expand(1.3f);
-		uiFont.expand(1.2f);
-		extraFont.expand(1.8f);
+		extraFont.expand(2);
+		inputFont.expand(1.5f);
 	}
-	//public static NText inputFont = new NText(symbols, ?, ?, ?);
-	//public static NText monsterFont = new NText(symbols, ?, ?, ?);
 	
 }
