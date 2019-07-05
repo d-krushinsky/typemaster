f081d853f21f3b567cb51eef85d6b130674478ee
dyuzz16 <ciclopej@gmail.com>
2019-07-05 10:37:19 +0300

    initialize V

--- src/game/entity/Monster.java
+++ src/game/entity/Monster.java
@@ -24,6 +24,7 @@ public class Monster extends Entity {
 		this.name = name;
 		setWidth(type.getWidth());
 		setHeight(type.getHeight());
+		this.v = type.V();
 		if(sprite != null) animator.start();
 	}
 	
